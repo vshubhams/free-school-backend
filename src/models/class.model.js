251eb6116fdbc860e4c0da07756c0c4b3254bcaa
shubham <shubhamv430@gmail.com>
2021-10-08 23:16:54 +0530

    update

--- src/models/class.model.js
+++ src/models/class.model.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const classSchema = new mongoose.Schema({
     title: { type: String, required: true },
     subject: { type: String, required: false },
-    class:{type:String,required:false},
+    class: { type: String, required: false },
+    time:{type:String,required:true},
     teacher: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "teacher",
