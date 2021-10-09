72bd4073694e3da5213a2a374a94bbb54acbee0b
shubham <shubhamv430@gmail.com>
2021-10-09 11:09:12 +0530

    get all teachers and students separately

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,8 +5,20 @@ const router = express.Router();
 const User = require("../models/user.model");
 
 router.get("", async function (req, res) {
-    const users = await User.find();
+    const users = await User.find().lean().exec();
     return res.status(201).json({ users });
+});
+
+// get all the students
+router.get("/students", async function (req, res) {
+    const students = await User.find({role:"student"}).lean().exec();
+    return res.status(201).json({ students });
+});
+
+// get all the teachers
+router.get("/teachers", async function (req, res) {
+    const teachers = await User.find({role:"teacher"}).lean().exec();
+    return res.status(201).json({ teachers });
 });
 
 module.exports = router
