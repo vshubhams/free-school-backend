9c66333d75d0c2c8852a895dc42c8e5b4ed58412
ravishukla86044 <ravishukls86044@gmail.com>
2021-10-09 14:33:20 +0530

    msg functionlity added

--- src/server.js
+++ src/server.js
@@ -5,11 +5,14 @@ const app = express();
 app.use(express.json());
 
 const { register, login } = require("./controllers/auth.controller");
+const Msg = require("./models/msg.model");
+const Chatroom = require("./models/chatroom.model");
 
 const userController = require("./controllers/user.controller");
 const classController = require("./controllers/class.controller");
 const childrenController = require("./controllers/children.controller");
-
+const chatroomController = require("./controllers/chatroom.controller");
+const msgController = require("./controllers/msg.controller");
 
 app.post("/register", register);
 app.post("/login", login);
@@ -17,13 +20,61 @@ app.post("/login", login);
 app.use("/users", userController);
 app.use("/classes", classController);
 app.use("/children", childrenController);
+app.use("/chatroom", chatroomController);
+app.use("/msg", msgController);
 
 const port = process.env.PORT || 3001;
-app.listen(port, async function () {
-    try {
-        await connect();
-        console.log(`Listen to port ${port}`);
-    } catch (error) {
-        console.log("error", error);
-    }
+const server = app.listen(port, async function () {
+  try {
+    await connect();
+    console.log(`Listen to port ${port}`);
+  } catch (error) {
+    console.log("error", error);
+  }
+});
+
+const io = require("socket.io")(server);
+
+let user = [];
+const addUser = (userId, socketId) => {
+  !user.some((user) => user.userId === userId) && user.push({ userId, socketId });
+};
+
+const removeAddedUser = (socketId) => {
+  user = user.filter((u) => u.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  console.log(user, "user");
+  return user.find((u) => u.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //connected
+  console.log("a user connected");
+  io.emit("welcome", "hello user from socket");
+
+  // take userId and socket from user
+  socket.on("addedUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", user);
+  });
+
+  //send Message and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text, currentChatroomId }) => {
+    const userParticular = getUser(receiverId);
+    console.log(userParticular, "parit");
+    console.log(senderId, receiverId, text, currentChatroomId, "msg");
+    io.emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //disconnected
+  socket.on("disconnect", () => {
+    console.log("user disconected");
+    removeAddedUser(socket.id);
+    io.emit("getUsers", user);
+  });
 });
