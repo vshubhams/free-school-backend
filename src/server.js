005e8b46dc8cb9604ff49402b8d9aa7f050344ff
ravishukla86044 <ravishukls86044@gmail.com>
2021-10-09 14:59:46 +0530

    cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const connect = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const { register, login } = require("./controllers/auth.controller");
 const Msg = require("./models/msg.model");
